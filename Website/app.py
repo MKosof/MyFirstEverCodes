bb0b09ddf7463e26b6e5b4d8ed93e83c2ace117a
mario <>
2023-12-21 13:49:49 +0200

    website with 2 pages, can take input

--- Website/app.py
+++ Website/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template,request,url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World </p>"
+@app.route("/" , methods=['GET', 'POST'])
+def index():
+    if "name" in request.args:
+        name_variable =", " + request.args["name"]
+    else:
+        name_variable=""
+    return render_template("index.html" , name_placeholder=name_variable)
+#Here we will have a key value pair in the url ?name="some value" and desplay it onto the website
+#The "name" in request.args is the key, the name variable and name placeholder could also be called "name", but for clarity's sake i'll leave it as is
+
+
+@app.route("/music")
+def route2():
+    return render_template("music.html")
+
